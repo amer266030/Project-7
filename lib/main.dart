a53e2c61c835d893113588972c4a69cc371b0257
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-21 11:00:54 +0300

    Update main.dart
    BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tuwaiq_project_pulse/model/enums/theme_state.dart';
+import 'package:tuwaiq_project_pulse/screens/settings/app_theme/app_theme_cubit.dart';
 import 'package:tuwaiq_project_pulse/screens/splash/splash_screen.dart';
 import 'package:tuwaiq_project_pulse/services/di_container.dart';
 
@@ -13,8 +16,35 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // theme: ThemeData(
+    return BlocProvider(
+      create: (context) => AppThemeCubit()..changeTheme(ThemeState.initial),
+      child: BlocBuilder<AppThemeCubit, AppThemeState>(
+        builder: (context, state) {
+          if (state is AppLightThemeState) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData.light(),
+              home: SplashScreen(),
+            );
+          } else if (state is AppDarkThemeState) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData.dark(),
+              home: SplashScreen(),
+            );
+          }
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: SplashScreen(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+/*
+// theme: ThemeData(
       //   colorScheme: const ColorScheme.light(
       //     primary: Colors.deepPurple,
       //     onPrimary: Colors.white,
@@ -35,8 +65,4 @@ class MainApp extends StatelessWidget {
       //   ),
       // ),
       // themeMode: ThemeMode.system,
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    );
-  }
-}
+*/
