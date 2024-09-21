34bb3a436759b4c317ed31c5e2b17c999154600b
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-21 13:34:32 +0300

    ADD BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tuwaiq_project_pulse/screens/settings/app_theme/app_theme_cubit.dart';
 import 'package:tuwaiq_project_pulse/screens/splash/splash_screen.dart';
 import 'package:tuwaiq_project_pulse/services/di_container.dart';
 
@@ -13,30 +15,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // theme: ThemeData(
-      //   colorScheme: const ColorScheme.light(
-      //     primary: Colors.deepPurple,
-      //     onPrimary: Colors.white,
-      //     secondary: Colors.deepPurpleAccent,
-      //     onSecondary: Color(0xFFFAFAFA),
-      //     surface: Color(0xFFFAFAFA),
-      //     onSurface: Colors.black,
-      //   ),
-      // ),
-      // darkTheme: ThemeData(
-      //   colorScheme: const ColorScheme.dark(
-      //     primary: Colors.deepPurple,
-      //     onPrimary: Colors.white,
-      //     secondary: Colors.deepPurpleAccent,
-      //     onSecondary: Color(0xFFFAFAFA),
-      //     surface: Color(0xFFFAFAFA),
-      //     onSurface: Colors.black,
-      //   ),
-      // ),
-      // themeMode: ThemeMode.system,
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+    return BlocProvider(
+      create: (context) => AppThemeCubit(),
+      child: BlocBuilder<AppThemeCubit, AppThemeState>(
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: SplashScreen(),
+          );
+        },
+      ),
     );
   }
 }
