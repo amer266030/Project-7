17c1fd5ac70c8522a36979c3ffcb36dc0acbedda
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-22 22:26:07 +0300

    Update rating_stars_view.dart

--- lib/reusable_components/rating_stars_view.dart
+++ lib/reusable_components/rating_stars_view.dart
@@ -12,17 +12,17 @@ class RatingStarsView extends StatelessWidget {
     return Row(
       children: List.generate(totalStars, (index) {
         if (rating >= index + 1) {
-          return const Icon(
+          return  Icon(
             Icons.star,
             color: C.primary(context),
           );
         } else if (rating >= index + 0.5) {
-          return const Icon(
+          return  Icon(
             Icons.star_half,
             color: C.primary(context),
           );
         } else {
-          return const Icon(
+          return  Icon(
             Icons.star_border,
             color: C.primary(context),
           );
